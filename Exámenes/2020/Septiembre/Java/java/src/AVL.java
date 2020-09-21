894f33ff8e0f52a75d8f9ff15155c656f1a8f771
Galán <antoniogalanG15@gmail.com>
2020-09-21 13:43:23 +0200

    Examen de Septiembre de 2020 (java) modificado
    Primer intento, sin comprobaciones.

--- Exámenes/2020/Septiembre/Java/java/src/AVL.java
+++ Exámenes/2020/Septiembre/Java/java/src/AVL.java
@@ -4,8 +4,10 @@
  * Student's group:
  */
 
+import dataStructures.list.ArrayList;
 import dataStructures.list.List;
 import dataStructures.list.LinkedList;
+import dataStructures.list.ListException;
 
 import java.util.Iterator;
 
@@ -15,18 +17,25 @@ class Bin {
     private List<Integer> weights; // weights of objects included in this bin
 
     public Bin(int initialCapacity) {
-        // todo
+        remainingCapacity = initialCapacity;
+        weights = new LinkedList<>();
     }
 
     // returns capacity left for this bin
     public int remainingCapacity() {
-        // todo
-        return 0;
+
+        return remainingCapacity;
     }
 
     // adds a new object to this bin
     public void addObject(int weight) {
-        // todo
+        if (remainingCapacity < weight) {
+            throw new ListException("Error con la capadidad");
+
+        } else {
+            remainingCapacity -= weight;
+            weights.append(weight);
+        }
     }
 
     // returns an iterable through weights of objects included in this bin
@@ -59,29 +68,66 @@ public class AVL {
 
         // recomputes height of this node
         void setHeight() {
-            // todo
+
+            if (left == null && right == null) {
+                height = 1;
+
+            } else if (left == null) {
+                height = 1 + right.height;
+
+            } else if (right == null) {
+                height = 1 + left.height;
+
+            } else {
+                height = 1 + Math.max(left.height, right.height);
+            }
         }
 
         // recomputes max capacity among bins in tree rooted at this node
         void setMaxRemainingCapacity() {
-            // todo
+
+            if (left == null && right == null) {
+                maxRemainingCapacity = bin.remainingCapacity();
+
+            } else if (left == null) {
+                maxRemainingCapacity = right.maxRemainingCapacity;
+
+            } else if (right == null) {
+                maxRemainingCapacity = left.maxRemainingCapacity;
+
+            } else {
+                maxRemainingCapacity = Math.max(left.maxRemainingCapacity, right.maxRemainingCapacity);
+            }
         }
 
         // left-rotates this node. Returns root of resulting rotated tree
         Node rotateLeft() {
-            // todo
-            return null;
+            Node res = this;
+
+            if (left == null) {
+                res.left = this;
+                res.right = null;
+
+            } else {
+                res.right = right.right;
+                res.left.right = right.left;
+                res.left.left = left;
+            }
+
+            res.setHeight();
+            res.setMaxRemainingCapacity();
+
+            return res;
         }
     }
 
     private static int height(Node node) {
-        // todo
-        return 0;
+        return node.height;
     }
 
     private static int maxRemainingCapacity(Node node) {
-        // todo
-        return 0;
+
+        return node.maxRemainingCapacity;
     }
 
     private Node root; // root of AVL tree
@@ -93,21 +139,90 @@ public class AVL {
     // adds a new bin at the end of right spine.
     private void addNewBin(Bin bin) {
         // todo
+        Node nodo = new Node();
+
+        if (this.root.right == null) {
+            nodo.left = null;
+            nodo.right = null;
+
+            nodo.bin = bin;
+
+            nodo.setHeight();
+            nodo.setMaxRemainingCapacity();
+
+//            if (nodo.right.height > (nodo.right.height +1)) {
+//                nodo.rotateLeft();
+//            }
+
+        } else {
+            this.root = this.root.right;
+            addNewBin(bin);
+
+//            if (this.root.right.height > (this.root.right.height +1)) {
+//                this.root.rotateLeft();
+//            }
+        }
     }
 
     // adds an object to first suitable bin. Adds
     // a new bin if object cannot be inserted in any existing bin
     public void addFirst(int initialCapacity, int weight) {
         // todo
+        Node nodo = new Node();
+
+        Bin bin = new Bin(initialCapacity);
+        bin.addObject(weight);
+
+        nodo.bin = bin;
+
+        if (this.root == null) {
+            nodo.left = null;
+            nodo.right = null;
+
+            nodo.setMaxRemainingCapacity();
+            nodo.setHeight();
+
+        } else if (maxRemainingCapacity(root.left) >= maxRemainingCapacity(root.right)) {
+
+
+        } else if (maxRemainingCapacity(root) >= weight) {
+
+
+        } else {
+            addNewBin(bin);
+        }
     }
 
     public void addAll(int initialCapacity, int[] weights) {
-        // todo
+
+        for (int w : weights) {
+            addFirst(initialCapacity, w);
+        }
     }
 
     public List<Bin> toList() {
         // todo
-        return null;
+        List<Bin> bins = new ArrayList<>();
+
+        // Izquierda
+        if (this.root.left == null) {
+            bins.append(this.root.bin);
+
+        } else {
+            this.root = this.root.left;
+            toList();
+        }
+
+        // Derecha
+        if (this.root.right == null) {
+            bins.append(this.root.bin);
+
+        } else {
+            this.root = this.root.right;
+            toList();
+        }
+
+        return bins;
     }
 
     public String toString() {
