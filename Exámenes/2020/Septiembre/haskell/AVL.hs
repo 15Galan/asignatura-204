07061a3088ce8f52dbdf28b4ab5fc6a224253884
Galán <antoniogalanG15@gmail.com>
2020-09-22 18:24:03 +0200

    Intento de resolución
    Aún queda.

--- Exámenes/2020/Septiembre/haskell/AVL.hs
+++ Exámenes/2020/Septiembre/haskell/AVL.hs
@@ -35,72 +35,70 @@ data Bin = B Capacity [Weight]
 data AVL = Empty | Node Bin Int Capacity AVL AVL deriving Show
 
 
+
+-- Un cubo con una capacidad dada, pero vacio
 emptyBin :: Capacity -> Bin
 emptyBin c = B c []
 
-
+-- La capacidad restante de un cubo ya viene indicada
 remainingCapacity :: Bin -> Capacity
 remainingCapacity (B c l) = c
 
-
+-- Si el objeto cabe en el cubo, lo inserta; si no, eleva un error
 addObject :: Weight -> Bin -> Bin
-addObject p (B c l) | (c-p) >= 0 = (B (c-p) (p:l))
+addObject 0 b = b
+addObject p (B c l) | (c-p) >= 0 = B (c-p) (p:l)
                     | otherwise  = error "El objeto no cabe en el cubo"
 
 
+-- Dado un arbol AVL, definimos su capacidad restante maxima como:
+-- La capacidad restante del cubo del arbol con mayor capacidad restante
 maxRemainingCapacity :: AVL -> Capacity
-maxRemainingCapacity Empty = 0
-maxRemainingCapacity (Node b h c izq der) = c
+maxRemainingCapacity (Node b h c Empty Empty) = c
+maxRemainingCapacity (Node b h c izq der) = maximum [c, maxRemainingCapacity izq, maxRemainingCapacity der]
 
-
+-- La altura de un cubo ya viene indicada
 height :: AVL -> Int
 height Empty = 0
+-- height (Node b h c izq der) = 1 + maximum (height izq, height der)
 height (Node b h c izq der) = h
 
-
+--
 nodeWithHeight :: Bin -> Int -> AVL -> AVL -> AVL
-nodeWithHeight b@(B c l) h Empty Empty = (Node b h c Empty Empty)
-nodeWithHeight b h Empty der = der
-nodeWithHeight b h izq Empty = izq
-nodeWithHeight b@(B c (l:ls)) h izq der | pesaIzq = nodeWithHeight bi hi izqi deri
-                                        | pesaDer = nodeWithHeight bd hd izqd derd
+nodeWithHeight b@(B c l) h izq der = Node b h c' izq der
   where
-    pesaIzq = c1 >= c2
-    pesaDer = c1 <  c2
-    (Node bi@(B c1 (li:lsi)) hi ci izqi deri) = izq
-    (Node bd@(B c2 (ld:lsd)) hd cd izqd derd) = der
+    c' = maxRemainingCapacity (Node b h c izq der)
 
 
 node :: Bin -> AVL -> AVL -> AVL
-node b@(B c l) Empty Empty = (Node b 1 c Empty Empty)
-node b@(B c l) Empty der = (Node b (1 + (height der)) c Empty der)
-node b@(B c l) izq Empty = (Node b (1 + (height izq)) c izq Empty)
-node b@(B c l) izq der = (Node b h' c' izq der)
+node b Empty Empty = Node b 1 (remainingCapacity b) Empty Empty
+node b izq der = nodeWithHeight b h' izq der
   where
-    h' = height izq + height der
-    c' = maxRemainingCapacity (Node b h'' c izq der)
-    h'' = maximum [(height izq), (height der)]
+    h' = 1 + maximum (height izq, height der)
 
 
--- Pendiente
 rotateLeft :: Bin -> AVL -> AVL -> AVL
-rotateLeft = undefined
--- rotateLeft b@(B c l) Empty Empty = (Node b 1 c Empty Empty)
--- rotateLeft b@(B c l) Empty der = (Node bd hd cd (Node bi (1 - (height der)) cd Empty Empty) izqd derd)
--- rotateLeft b@(B c l) izq Empty = (Node bi hi ci (Node bd (1 - (height izq)) ci Empty Empty) izqi deri)
--- rotateLeft b izq der = (Node bd (1+hd) cd izq izqi)
---   where
---     (Node bi@(B c1 (li:lsi)) hi ci izqi deri) = izq
---     (Node bd@(B c2 (ld:lsd)) hd cd izqd derd) = der
+rotateLeft b Empty Empty = Node b 1 (remainingCapacity b) Empty Empty
+rotateLeft b izq der = Node bd hd cd (Node b h' (remainingCapacity b) izq izqd) derd
+  where
+    h' = maximum (height izq, height der)
+    Node bi hi ci izqi deri = izq
+    Node bd hd cd izqd derd = der
 
 
 addNewBin :: Bin -> AVL -> AVL
-addNewBin b@(B c l) Empty = (Node b 1 c Empty Empty)
-addNewBin b@(B c l) (Node b2 h2 c2 izq der) = addNewBin b der
+addNewBin b (Node _ _ _ Empty Empty) = Node b 1 (remainingCapacity b) Empty Empty
+addNewBin b (Node b h c Empty der) = addNewBin b der
 -- Falta actualizar la altura
 
+
 addFirst :: Capacity -> Weight -> AVL -> AVL
-addFirst _ _ _ = undefined
+addFirst c p Empty = Node (addObject p $ emptyBin c) 1 c Empty Empty
+addFirst c p (Node b h cc izq der) | cabe      = addFirst c p izq
+                                   | not cabe  = addFirst c p der
+  where
+    cabe = p <= cc
+
 
 addAll:: Capacity -> [Weight] -> AVL
 addAll _ _ = undefined
